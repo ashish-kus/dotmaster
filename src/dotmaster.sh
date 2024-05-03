d54935010234fd58665dd3c154fc713ea05b9e2d
ashish-kus <ashish.kus2408@gmail.com>
2024-05-03 23:09:59 +0530

    added local sync option

--- src/dotmaster.sh
+++ src/dotmaster.sh
@@ -3,6 +3,7 @@
 set -oue pipefail
 # set -x pipefail
 DOTMASTER_LOG_FILE=${DOTMASTER_LOG_FILE:-$HOME/.dotmaster.log}
+# PACKAGE_LIST=("git" "curl" "wget" "zsh" "tmux" "foot")
 
 # color variables
 red='\033[0;31m'
@@ -60,38 +61,74 @@ setup_package_manager() {
         exit 1
     fi
 }
-
-# Function to check if a command exists
-command_exists() {
-    command -v "$1" >/dev/null 2>&1
-}
-
-# Function to install packages
 install_packages() {
-  echo "installing packages---------------------------------------------"
     local package_manager="$PACKAGE_MANAGER"
-    local packages=( "$@" )
-
+    local packages=( "$@" )  # Use local keyword for packages array
     if [ "${#packages[@]}" -eq 0 ]; then
-        echo "No packages specified to install."
+        print_error "No packages specified to install."
         return
     fi
-
     for package in "${packages[@]}"; do
-        if ! command_exists "$package"; then
-            _e "Uh-oh! $package is missing."
-            _a "Installing $package using $package_manager..."
-            if $INSTALL_COMMAND "$package" 2>&1; then
-                _s "$package installed using $package_manager"
-            else
-                _e "Failed to install $package."
-            fi
-        else
-            _a "$package is already installed."
-        fi
+        # if ! command_exists "$package"; then
+        #     _e "Uh-oh! $package is missing."
+        #     _a "Installing $package using $package_manager..."
+        #     yes | $INSTALL_COMMAND "$package" 2>&1 | _log "installing $package"
+        #     if [ $? -eq 0 ]; then
+        #         print_success "$package installed using $package_manager"
+        #     else
+        #         print_error "Failed to install $package."
+        #         exit 1
+        #     fi
+        # else
+        #     _s "$package is already installed."
+        # fi
+        echo $package
     done
-    # echo $PACKAGE_MANAGER $PACKAGE_INSTALL_COMMAND $PACKAGE_INSTALL_LIST
 }
+# install_packages() {
+#     local package_manager="$PACKAGE_MANAGER"
+#     local packages=( "$@" )  # Use local keyword for packages array
+#     if [ "${#packages[@]}" -eq 0 ]; then
+#         _e "No packages specified to install."
+#         return
+#     fi
+#     for package in "${packages[@]}"; do
+#         if ! command_exists "$package"; then
+#             _e "Uh-oh! $package is missing."
+#             _a "Installing $package using $package_manager..."
+#             yes | $INSTALL_COMMAND "$package" 2>&1 | _log "installing $package"
+#             if [ $? -eq 0 ]; then
+#                 print_success "$package installed using $package_manager"
+#             else
+#                 print_error "Failed to install $package."
+#                 exit 1
+#             fi
+#         else
+#             _s "$package is already installed."
+#         fi
+#     done
+# }
+# install_packages() {
+#     local package_manager="$PACKAGE_MANAGER"
+#     packages=( "$@" )
+#     echo $packages
+    # if [ "${#packages[@]}" -eq 0 ]; then
+    #     _e "No packages specified to install."
+    #     return
+    # fi
+    # for package in "${packages[@]}";
+    #     do
+    #       if ! command_exists "$package"; then
+    #         _e "Uh-oh! $package is missing."
+    #         _a "Installing $package using $package_manager"
+    #         yes | $INSTALL_COMMAND "$package" 2>&1 | _log "installing $package"
+    #         # $INSTALL_COMMAND $package 2>&1
+    #         if [ $? == 0 ]; then _s "$package installed using $package_manager" ;else _e "can't install $package"; (exit 1); fi;
+    #       else
+    #           _s "$package is already installed."
+    #       fi
+    # done
+# }
 
 installing_repo() {
     if [ -d "$DOTFILES_PATH" ]; then
@@ -173,7 +210,6 @@ DOTFILES_PATH="$(eval echo "$DOTFILES_PATH/.dotfiles")"
 export DOTFILES_PATH="$DOTFILES_PATH" # path might contain variables or special characters that 
                                       # need to be expanded or interpreted correctly.                                      
 CONFIG_PATH="${CONFIG_PATH:-$DOTFILES_PATH/config.ini}"
-
 setup_package_manager
 if ! command_exists "git";then
     install_packages "git"
@@ -224,12 +260,10 @@ if $use_git_repo && $use_local_dir; then
     _e "Error: Both URL and directory options cannot be used together"
     exit 1
 elif $use_git_repo; then
-    # _s "Using git repo: $DOTFILE_GIT_REPO"
-    _s "Using git repo: "
-    _s $use_git_repo
+    _s "Using git repo: $DOTFILE_GIT_REPO"
     installing_repo $DOTFILE_GIT_REPO
 
-    if [ ! -f $CONFIG_PATH ]; then
+    if [ ! -f "$CONFIG_PATH" ]; then
         _e "config not found at $CONFIG_PATH"
         _a "Creating config_ini at $CONFIG_PATH"
         echo -e "$default_config" > "$CONFIG_PATH"
@@ -237,20 +271,49 @@ elif $use_git_repo; then
     else
         _s "config found at $CONFIG_PATH"
     fi
-    parse_ini $CONFIG_PATH
-    install_packages $PACKAGE_INSTALL_LIST
-    create_symlinks $DOTFILES_PATH
-    # echo $DOTFILE_GIT_REPO
-    elif $use_local_dir; then
-      _s "Using local directory: "
-      _s $use_local_dir
-      _s "update synk"
-      parse_ini $CONFIG_PATH
-      install_packages $PACKAGE_INSTALL_LIST
-
-      create_symlinks $DOTFILES_PATH
-      # _s "Using local directory: $DOTFILES_PATH"
+    parse_ini "$CONFIG_PATH"
+    install_packages "${PACKAGE_INSTALL_LIST[@]}"
+    create_symlinks "$DOTFILES_PATH"
+elif $use_local_dir; then
+    _s "Using local directory: $DOTFILES_PATH"
+    parse_ini "$CONFIG_PATH"
+    install_packages "${PACKAGE_INSTALL_LIST[@]}"
+    create_symlinks "$DOTFILES_PATH"
 fi
+
+
+
+# ----------------------------------------------------------------------------
+# if $use_git_repo && $use_local_dir; then
+#     _e "Error: Both URL and directory options cannot be used together"
+#     exit 1
+# elif $use_git_repo; then
+#     # _s "Using git repo: $DOTFILE_GIT_REPO"
+#     _s "Using git repo: "
+#     _s $use_git_repo
+#     installing_repo $DOTFILE_GIT_REPO
+
+#     if [ ! -f $CONFIG_PATH ]; then
+#         _e "config not found at $CONFIG_PATH"
+#         _a "Creating config_ini at $CONFIG_PATH"
+#         echo -e "$default_config" > "$CONFIG_PATH"
+#         _s "config created successfully "
+#     else
+#         _s "config found at $CONFIG_PATH"
+#     fi
+#     parse_ini $CONFIG_PATH
+#     install_packages $PACKAGE_INSTALL_LIST
+#     create_symlinks $DOTFILES_PATH
+#     # echo $DOTFILE_GIT_REPO
+#     elif $use_local_dir; then
+#       _s "Using local directory: "
+#       _s $use_local_dir
+#       parse_ini $CONFIG_PATH
+#       install_packages $PACKAGE_INSTALL_LIST
+#       _s "update synk"
+#      create_symlinks $DOTFILES_PATH
+#     # _s "Using local directory: $DOTFILES_PATH"
+# fi
 
 _s "All set! Terminal, take charge! 🛠️💻"
 }
